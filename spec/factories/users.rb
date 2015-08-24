4c502e60221bc0d58e013e96bdd6e749770ff0d1
Ivor Padilla <ivorjpc@gmail.com>
2015-08-23 23:04:43 -0430

    Tests passed

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :user do
     name "Admin Example"
     #sequence(:email, 100) { |n| "person#{n}@example.com" }
-    email "admin@example2.com"
+    email "admin@example.com"
     password "12345678"
     password_confirmation "12345678"
     confirmed_at Time.now
--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,88 +1,80 @@
 require 'rails_helper'
 
-  describe User do
-
-    describe "sign_up" do
-      before do
-        visit new_user_registration_path
-      end
-
-      it "can sign up and receives confirmation email" do
-        fill_in 'Name', with: 'Ivor'
-        fill_in 'Email', with: 'example@exampless.com'
-        fill_in 'Password', with: '12345678'
-        fill_in 'Password confirmation', with: '12345678'
-        click_button 'Sign up'
-        expect(page).to have_content('A message with a confirmation link has been sent to your email address. Please follow the link to activate your account.')
-      end
-
-      it "has invalid email" do
-        fill_in 'Name', with: 'Ivor'
-        fill_in 'Email', with: 'ivorpad@123'
-        fill_in 'Password', with: '12345678'
-        fill_in 'Password confirmation', with: '12345678'
-        click_button 'Sign up'
-        expect(page).to have_content('Email is invalid')
-      end
-
-      it "has duplicated email" do
-        fill_in 'Name', with: 'Ivor'
-        fill_in 'Email', with: 'admin@example.com'
-        fill_in 'Password', with: '12345678'
-        fill_in 'Password confirmation', with: '12345678'
-        click_button 'Sign up'
-        expect(page).to have_content('Email has already been taken')
-      end
-
+describe User do
+  describe 'sign_up' do
+    before do
+      visit new_user_registration_path
     end
 
-    describe "successful" do
-
-      before do
-        visit new_user_session_path
-        @user = build(:user)
-      end
-
-      it "can sign in" do
-        fill_in 'Email', with: @user.email
-        fill_in 'Password', with: @user.password
-
-        within 'form' do
-          click_button 'Sign in'
-        end
-        expect(page).to have_content('Signed in successfully')
-      end
+    it 'can sign up and receives confirmation email' do
+      fill_in 'Name', with: 'Ivor'
+      fill_in 'Email', with: 'example@exampless.com'
+      fill_in 'Password', with: '12345678'
+      fill_in 'Password confirmation', with: '12345678'
+      click_button 'Sign up'
+      expect(page).to have_content('A message with a confirmation link has been sent to your email address. Please follow the link to activate your account.')
     end
 
-    describe "sign out" do
-
-      before do
-        visit new_user_session_path
-        @user = build(:user)
-      end
-
-      it "sign out successfully" do
-        fill_in 'Email', with: @user.email
-        fill_in 'Password', with: @user.password
-        click_button 'Sign in'
-        click_on 'Sign out'
-        expect(page).to have_content('Signed out successfully.')
-      end
+    it 'has invalid email' do
+      fill_in 'Name', with: 'Ivor'
+      fill_in 'Email', with: 'ivorpad@123'
+      fill_in 'Password', with: '12345678'
+      fill_in 'Password confirmation', with: '12345678'
+      click_button 'Sign up'
+      expect(page).to have_content('Email is invalid')
     end
 
-    describe "reset" do
-      before do
-        visit new_user_password_path
-        @user1 = build(:user)
-
-      end
-      it "sends instructions" do
-        fill_in 'Email', with: @user1.email
-        click_button 'Send me reset password instructions'
-        expect(page).to have_content('You will receive an email with instructions on how to reset your password in a few minutes.')
-      end
+    it 'has duplicated email' do
+      fill_in 'Name', with: 'Ivor'
+      fill_in 'Email', with: 'admin@example.com'
+      fill_in 'Password', with: '12345678'
+      fill_in 'Password confirmation', with: '12345678'
+      click_button 'Sign up'
+      expect(page).to have_content('Email has already been taken')
     end
-
-
-
   end
+
+  describe 'successful' do
+    before do
+      visit new_user_session_path
+      @user = build(:user)
+    end
+
+    it 'can sign in' do
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
+
+      within 'form' do
+        click_button 'Sign in'
+      end
+      expect(page).to have_content('Signed in successfully')
+    end
+  end
+
+  describe 'sign out' do
+    before do
+      visit new_user_session_path
+      @user = build(:user)
+    end
+
+    it 'sign out successfully' do
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
+      click_button 'Sign in'
+      click_on 'Sign out'
+      expect(page).to have_content('Signed out successfully.')
+    end
+  end
+
+  describe 'reset' do
+    before do
+      visit new_user_password_path
+      @user1 = build(:user)
+    end
+    it 'sends instructions' do
+      fill_in 'Email', with: @user1.email
+      click_button 'Send me reset password instructions'
+      expect(page).to have_content('You will receive an email with instructions on how to reset your password in a few minutes.')
+    end
+  end
+end
