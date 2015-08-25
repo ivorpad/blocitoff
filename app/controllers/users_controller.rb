72cd4581e3581032b9f60ef70248a25bb6a63422
Ivor Padilla <ivorjpc@gmail.com>
2015-08-24 22:21:06 -0430

    Added the add todo items feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    #@items = Item.find(params[:user_id])
+    @items = @user.items
+    @item = Item.new
   end
 
-  def index
+  def new
   end
 
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ActiveRecord::Base
   belongs_to :user
+  default_scope { order('updated_at DESC') }
+
+  validates :name, length: { minimum: 5 }
 end
