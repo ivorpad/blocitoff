776822aad025c1d19f3bcaccb0bb16d70a1371ad
Ivor Padilla <ivorjpc@gmail.com>
2015-08-26 22:10:04 -0430

    Finished the time remaining assignment

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,9 @@ class Item < ActiveRecord::Base
   default_scope { order('updated_at DESC') }
 
   validates :name, length: { minimum: 5 }
+
+  def days_left
+      7 - (DateTime.now.to_date - created_at.to_date).to_i
+  end
+
 end
